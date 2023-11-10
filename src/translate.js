b57bd3af5a1480eee960053261dfdf95ea81c7bc
sleefd <sleefd@gmail.com>
2023-11-11 00:19:46 +0800

    refactor func to a class
    * learn to use js class

--- src/translate.js
+++ src/translate.js
@@ -1,57 +1,64 @@
-// default options
-let options = {
-  lang: ["English", "Chinese"],
-  selector: "post",
-  endpoint: "http://localhost:8080/translate",
-  callback: callback,
-};
+class TranslateIt {
+  static defaultOptions = {
+    lang: ["English", "Chinese", "French"],
+    selector: ".post",
+    endpoint: "http://localhost:8080/translate",
+    callback: (json) => json.reply
+  };
 
-function translate(selector, options = {}) {
-  const _selector = selector || options.selector;
-  let block = document.querySelector(_selector);
-  let select = document.createElement("select");
-  select.classList.add("translate-btn");
-  select.addEventListener("change", (event) => {
-    let lang = event.target.value;
-    translateElement(block, options, lang);
-  });
-  const langs = options.lang;
-  let option = document.createElement("option");
-  option.text = "translate";
-  option.classList.add("translate-option", "selected", "disabled");
-  select.appendChild(option);
-  for (let i = 0; i < langs.length; i++) {
-    let option = document.createElement("option");
-    option.text = langs[i];
-    option.classList.add("translate-option");
-    select.appendChild(option);
+  constructor(selector, options) {
+    this.selector = selector;
+    this.options = options;
   }
-  block.insertAdjacentElement("beforebegin", select);
-}
 
-function translateElement(element, options, lang) {
-  fetch(options.endpoint, {
-    method: "POST", body: JSON.stringify({
-      lang: lang,
-      content: element.innerHTML
-    })
-  })
-  .then(resp => resp.json())
-  .then(json => {
-    let content = options.callback(json);
-    if (content) {
-      element.innerHTML = content;
-    } else {
-      console.error("error response", json);
+  translate() {
+    this.options = Object.assign({}, TranslateIt.defaultOptions, this.options);
+    const selector = this.selector || this.options.selector;
+    let block = document.querySelector(selector);
+    let select = document.createElement("select");
+    select.classList.add("translate-btn");
+    select.addEventListener("change", (event) => {
+      let lang = event.target.value;
+      this.translateElement(block, this.options, lang);
+    });
+    const langs = this.options.lang;
+    let option = document.createElement("option");
+    option.text = "translate";
+    option.classList.add("translate-option", "selected", "disabled");
+    select.appendChild(option);
+    for (let i = 0; i < langs.length; i++) {
+      let option = document.createElement("option");
+      option.text = langs[i];
+      option.classList.add("translate-option");
+      select.appendChild(option);
     }
-  });
+    block.insertAdjacentElement("beforebegin", select);
+  }
+
+  translateElement(element, options, lang) {
+    fetch(options.endpoint, {
+      method: "POST", body: JSON.stringify({
+        lang: lang,
+        content: element.innerHTML
+      })
+    })
+    .then(resp => resp.json())
+    .then(json => {
+      let content = options.callback(json);
+      if (content) {
+        element.innerHTML = content;
+      } else {
+        console.error("error response", json);
+      }
+    });
+  }
 }
 
-// defines how to parse the json resp from the endpoint
-// return the result of translation
-function callback(json) {
-  return json.reply;
-}
+const translateIt = {
+  translate: function (selector, options) {
+    new TranslateIt(selector, options).translate();
+  }
+};
 
 if (typeof module != "undefined") {
   module.exports = translate;
