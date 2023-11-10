1d7904c567dfc5b14c6b60b7bddb8d6e1d32ece9
sleefd <sleefd@gmail.com>
2023-11-11 00:45:47 +0800

    fix eslint problems

--- src/translate.js
+++ src/translate.js
@@ -1,10 +1,10 @@
 class TranslateIt {
 
   static defaultOptions = {
-    lang: ["English", "Chinese", "French"],
-    selector: ".post",
-    endpoint: "http://localhost:8080/translate",
-    callback: (json) => json.reply
+    lang: ['English', 'Chinese', 'French'],
+    selector: '.post',
+    endpoint: 'http://localhost:8080/translate',
+    callback: (json) => json.reply,
   };
 
   constructor(selector, options) {
@@ -18,23 +18,23 @@ class TranslateIt {
     const selector = this.selector || this.options.selector;
     const block = document.querySelector(selector);
 
-    const select = document.createElement("select");
-    select.classList.add("translate-btn");
-    select.addEventListener("change", (event) => {
+    const select = document.createElement('select');
+    select.classList.add('translate-btn');
+    select.addEventListener('change', (event) => {
       const lang = event.target.value;
       this.translateElement(block, this.options, lang);
     });
 
-    this.createOption(select, "Translate", ["selected"]);
+    this.createOption(select, 'Translate', ['selected']);
     for (const lang of this.options.lang) {
-      this.createOption(select, lang, ["translate-option"]);
+      this.createOption(select, lang, ['translate-option']);
     }
 
-    block.insertAdjacentElement("beforebegin", select);
+    block.insertAdjacentElement('beforebegin', select);
   }
 
   createOption(select, text, classes) {
-    const option = document.createElement("option");
+    const option = document.createElement('option');
     option.text = text;
     classes.forEach(name => option.classList.add(name));
     select.appendChild(option);
@@ -42,29 +42,27 @@ class TranslateIt {
 
   translateElement(element, options, lang) {
     fetch(options.endpoint, {
-      method: "POST", body: JSON.stringify({
+      method: 'POST', body: JSON.stringify({
         lang: lang,
-        content: element.innerHTML
-      })
-    })
-    .then(resp => resp.json())
-    .then(json => {
+        content: element.innerHTML,
+      }),
+    }).then(resp => resp.json()).then(json => {
       let content = options.callback(json);
       if (content) {
         element.innerHTML = content;
       } else {
-        console.error("error response", json);
+        console.error('error response', json);
       }
     });
   }
 }
 
 const translateIt = {
-  translate: function (selector, options) {
+  translate: function(selector, options) {
     new TranslateIt(selector, options).translate();
-  }
+  },
 };
 
-if (typeof module != "undefined") {
-  module.exports = translate;
+if (typeof module != 'undefined') {
+  module.exports = translateIt;
 }
